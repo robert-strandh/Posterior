6b220db0ad6abc398ec37df8bb28d3faaedecf29
Robert Strandh <robert.strandh@gmail.com>
2024-06-22 15:21:21 +0200

    Export INTEGER-ADD/SUBTRACT-INSTRUCTION and their accessors.

--- Code/MIR-instructions/packages.lisp
+++ Code/MIR-instructions/packages.lisp
@@ -10,6 +10,8 @@
    #:binary-operation-mixin
    #:unary-operation-mixin
    #:commutative-mixin
+   #:integer-add-instruction #:augend #:addend
+   #:integer-subtract-instruction #:minuend #:subtrahend
    #:binary-bitwise-instruction
    #:bitwise-and-instruction
    #:bitwise-or-instruction
