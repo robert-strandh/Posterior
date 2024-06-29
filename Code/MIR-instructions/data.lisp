dc0af4bcb3fdbcaf71ea4ca377515e72c88ad915
Robert Strandh <robert.strandh@gmail.com>
2024-06-29 09:29:09 +0200

    Remove obsolete slot.

--- Code/MIR-instructions/data.lisp
+++ Code/MIR-instructions/data.lisp
@@ -9,10 +9,6 @@
 
 (defgeneric size (virtual-register))
 
-(defgeneric nature (virtual-register))
-
 (defclass virtual-register (datum)
   (;; This slot contains the size in bits of the virtual register.
-   (%size :initarg :size :reader size)
-   ;; This slot contains either :SIGNED or :UNSIGNED.
-   (%nature :initarg :nature :reader nature)))
+   (%size :initarg :size :reader size)))
