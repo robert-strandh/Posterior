dd57a6c24de9a83cb4c2d0f551cd6425c3b5c257
Robert Strandh <robert.strandh@gmail.com>
2024-07-03 09:24:12 +0200

    Add comment.

--- Code/MIR-instructions/new-virtual-register-instruction.lisp
+++ Code/MIR-instructions/new-virtual-register-instruction.lisp
@@ -1,5 +1,10 @@
 (cl:in-package #:posterior-mir-instructions)
 
+;;; This instruction is used to introduce a new virtual register.  It
+;;; must dominate every instruction that references or assigns to the
+;;; virtual register being introduced.  The initial value can be a
+;;; literal or an existing virtual register.
+
 (defclass new-virtual-register-instruction
     (one-successor-mixin instruction)
   ((%initial-value :initarg :initial-value :reader initial-value)
