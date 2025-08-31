e3c6059a0c9c3fc0fcdd5866473d3bced032fd6b
Qiantan Hong <qthong@stanford.edu>
2025-08-31 07:20:13 -0700

    memory-read/write-instruction takes single address

--- Code/MIR-instructions/memory-read-instruction.lisp
+++ Code/MIR-instructions/memory-read-instruction.lisp
@@ -1,13 +1,10 @@
 (cl:in-package #:posterior-mir-instructions)
 
-;;; This instruction reads a memory location.  It takes two inputs.
-;;; The first input is the base-address of a location in memory.  The
-;;; second input is an offset to be added to the base address.  It has
-;;; a single output which is set to the contents of the memory
-;;; location at the address specified by the input and the offset
-;;; added together.  The offset must be a literal datum.
+;;; This instruction reads a memory location.  It has a single input
+;;; which is the address of a location in memory.  It has a single
+;;; output which is set to the contents of the memory location
+;;; specified by the input.
 
 (defclass memory-read-instruction (instruction one-successor-mixin)
-  ((%base-address :initarg :base-address :reader base-address)
-   (%offset :initarg :offset :reader offset)
+  ((%address :initarg :address :reader address)
    (%datum :initarg :datum :reader datum)))
